7dbedbb37ae42fe124f195d81755baa863a8f1a6
jj225511 <admin@zyzh.cc>
2021-12-09 22:13:33 +0800

    Update AutoBuild_DiyScript.sh

--- Scripts/AutoBuild_DiyScript.sh
+++ Scripts/AutoBuild_DiyScript.sh
@@ -12,7 +12,7 @@ Diy_Core() {
 	Load_CustomPackages_List=true
 	Short_Firmware_Date=true
 	Checkout_Virtual_Images=false
-	Firmware_Format=false
+	Firmware_Format=AUTO
 	REGEX_Skip_Checkout="packages|buildinfo|sha256sums|manifest|kernel|rootfs|factory"
 
 	INCLUDE_AutoBuild_Features=true
